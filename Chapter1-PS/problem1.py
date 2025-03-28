f08c2c22538bce6f3197a5505a19ecdb93577b8c
Prateek <Prateekrajgrd74@gmail.com>
2025-03-29 01:01:50 +0530

    python

---
+++ Chapter1-PS/problem1.py
@@ -0,0 +1,28 @@
+print('''Twinkele, twinkle, little star,
+        How I wonder what you are!
+        Up above the world so high,
+        Like a diamond in the sky.
+      
+        when the blazing sun is gone,
+        when he nothing shines upon,
+        then you show your little light,
+        twinkle, twinkle, all the night.
+      
+        then the traveler in the dark,
+        thanks you for your tiny spark;
+        he could not see which way to go,
+        if you did not twinkle so.
+        
+        in the dark blue sky you keep,
+        and often through my curtains peep,
+        for you never shut your eye,
+        till the sun is in the sky.
+        
+        tis your little light that guides,
+        and often through my curtains peep,
+        for you never shut your eye,
+        till the sun is in the sky.
+        
+        how I wonder what you are!
+        twinkle, twinkle, little star,
+        ''')
