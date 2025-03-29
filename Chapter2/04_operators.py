b2da13357c362a95ad5e42f1c6e985cb32266e6f
Prateek <Prateekrajgrd74@gmail.com>
2025-03-29 19:23:27 +0530

    python

---
+++ Chapter2/04_operators.py
@@ -0,0 +1,43 @@
+#Arithmetic Operators
+
+a = 34
+b = 4
+c = a+b
+
+print(c)
+
+#Assignment Operators
+
+a = 4-2
+print(a)
+b = 6
+b += 3   # increment b by 3 and then assign it to b
+print(b)
+b -= 3   # decrement b by 3 and then assign it to b
+print(b)
+b *= 3   # multiply b by 3 and then assign it to b
+print(b)
+b /= 3   # divide b by 3 and then assign it to b
+print(b)
+
+# Comparison Operators
+
+d = 5 < 4
+print(d)  # False
+
+e = 6 < 7
+print(e)  # True
+
+f = 5 == 5
+print(f)  # True
+
+g = 5 != 5
+print(g)  # False
+
+#logical operators
+
+h = True and False
+print(h)  # False
+
+i = True or False
+print(i)  # True
